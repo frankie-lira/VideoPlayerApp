b84195ecfe366e282f12216b4ad14442c4952ae9
francisco <franciscolira408@gmail.com>
2023-01-19 13:03:30 -0800

    cretaed and added small changes

---
+++ VideoPlayerApp/VideoPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerAppApp.swift
+//  VideoPlayerApp
+//
+//  Created by Francisco Lira on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
